0c34a270fedaea4b679990007c8a6aa682e95e64
FouadElMetioui <fouadpro2002@gmail.com>
2024-10-22 12:05:34 +0100

    add application

---
+++ src/main/java/io/fouad/carrerbackend/application/Application.java
@@ -0,0 +1,32 @@
+package io.fouad.carrerbackend.application;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.fouad.carrerbackend.candidature.Candidature;
+import io.fouad.carrerbackend.offre.Offre;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Application {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "candidature_id", nullable = false)
+    @JsonIgnore
+    private Candidature candidature;
+
+    @ManyToOne
+    @JoinColumn(name = "offre_id", nullable = false)
+    @JsonIgnore
+    private Offre offre;
+}
---
+++ src/main/java/io/fouad/carrerbackend/application/ApplicationRepository.java
@@ -0,0 +1,7 @@
+package io.fouad.carrerbackend.application;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+
+}
